df0acc1461f00bec3eed3bfa6ed487bc05f03839
namanrox <naman.deep991@gmail.com>
2025-02-05 23:18:10 +0530

    interview start (landing) page created

--- app/dashboard/_components/AddNewInterview.jsx
+++ app/dashboard/_components/AddNewInterview.jsx
@@ -18,6 +18,7 @@ import { db } from "@/utils/db";
 import { v4 as uuidv4 } from "uuid";
 import { useUser } from "@clerk/nextjs";
 import moment from "moment";
+import { useRouter } from "next/navigation";
 
 function AddNewInterview() {
   const [openDialog, setOpenDialog] = useState(false);
@@ -26,7 +27,7 @@ function AddNewInterview() {
   const [jobExperience, setJobExperience] = useState();
   const [loading, setLoading] = useState(false);
   const [jsonResponse, setJsonResponse] = useState([]);
-
+  const router = useRouter();
   const { user } = useUser();
 
   const onSubmit = async (e) => {
@@ -71,6 +72,7 @@ function AddNewInterview() {
       console.log("Inserted ID:", resp);
       if (resp) {
         setOpenDialog(false);
+        router.push("/dashboard/interview/" + resp[0]?.mockId);
       }
     } else {
       console.log("ERROR");
